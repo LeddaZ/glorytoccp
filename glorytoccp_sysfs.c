25bd367cb7388573ac3852936ea468b3018c3bd6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-02 09:17:57 +0100

    glorytoccp: Move to sysfs_create_groups
    Change-Id: I973a90e29b7a136b03e1ccd081c9f7a53de4df6e

--- glorytoccp_sysfs.c
+++ glorytoccp_sysfs.c
@@ -11,27 +11,51 @@
 #include "glorytoccp.h"
 #include "glorytoccp_xjp.h"
 
+#define KOBJ_ATTR(_name, _mode, _show, _store) \
+	struct kobj_attribute kobj_attr_##_name = __ATTR(_name, _mode, _show, _store)
+#define KOBJ_ATTR_RW(_name) \
+	struct kobj_attribute kobj_attr_##_name = __ATTR_RW(_name)
+#define KOBJ_ATTR_RO(_name) \
+	struct kobj_attribute kobj_attr_##_name = __ATTR_RO(_name)
+
 static ssize_t oursupremeleader_show(struct kobject *kobj, 
 		struct kobj_attribute *attr, char *buf) {
 	return sprintf(buf, "%s", oursupremeleader);
 }
+static KOBJ_ATTR_RO(oursupremeleader);
 
-static struct kobj_attribute glorytoccp_attr = __ATTR(oursupremeleader, 0444,
-		oursupremeleader_show, NULL);
+static struct attribute *glorytoccp_attrs[] = {
+	&kobj_attr_oursupremeleader.attr,
+	NULL
+};
+
+static const struct attribute_group glorytoccp_group = {
+	.attrs = glorytoccp_attrs,
+};
+
+static const struct attribute_group *glorytoccp_groups[] = {
+	&glorytoccp_group,
+	NULL
+};
 
 static struct kobject *kobj;
 
 void glorytoccp_sysfs_init(void) {
+	int ret;
+
 	kobj = kobject_create_and_add(KBUILD_MODNAME, NULL);
 
-	if (sysfs_create_file(kobj, &glorytoccp_attr.attr))
-		LOGE("Cannot create sysfs file, our Supreme Leader won't be happy!\n");
-	
+	ret = sysfs_create_groups(kobj, glorytoccp_groups);
+	if (ret) {
+		LOGE("sysfs_create_group failed\n");
+		return;
+	}
+
 	return;
 }
 
 void glorytoccp_sysfs_exit(void) {
-	sysfs_remove_file(kobj, &glorytoccp_attr.attr);
+	sysfs_remove_groups(kobj, glorytoccp_groups);
 
 	kobject_put(kobj);
 
